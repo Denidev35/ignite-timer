6ab7ef5f439a1fd58c4c6e7b3708ee78cde3ba09
Denilson Silva <denilson20@hotmail.com>
2022-09-05 16:37:53 -0300

    finished

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,67 @@
+import { HandPalm, Play } from 'phosphor-react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import {
+  HomeContainer,
+  StartCountdonwButton,
+  StopCountdonwButton,
+} from './styles'
+import { Countdown } from './components/Countdown'
+import { NewCycleForm } from './components/NewCycleForm'
+import { useContext } from 'react'
+import { CyclesContext } from '../../contexts/CycleContext'
+
+const newCycleFormValidationsSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(5).max(60),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationsSchema>
+
+export function Home() {
+  const { activeCycle, createNewCycle, InterruptCurrentCycle } =
+    useContext(CyclesContext)
+
+  const newCycleForm = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationsSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisable = !task
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
+        <FormProvider {...newCycleForm}>
+          <NewCycleForm />
+        </FormProvider>
+        <Countdown />
+
+        {activeCycle ? (
+          <StopCountdonwButton onClick={InterruptCurrentCycle} type="button">
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdonwButton>
+        ) : (
+          <StartCountdonwButton type="submit" disabled={isSubmitDisable}>
+            <Play size={24} />
+            Começar
+          </StartCountdonwButton>
+        )}
+      </form>
+    </HomeContainer>
+  )
+}
